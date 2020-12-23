4d99fa1962aa0ae240d87f2e22aff503c65b533e
Daniel Thomas <63409207+DanielTk26@users.noreply.github.com>
2020-12-23 13:23:22 +0300

    Add files via upload

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, Image, TouchableOpacity,TextInput, Alert } from 'react-native';
+import { View, StyleSheet, Text, Image, TouchableOpacity,TextInput, Modal, KeyboardAvoidingView, Alert, ScrollView } from 'react-native';
 import SantaAnimation from '../components/SantaClaus.js';
 import db from '../config';
 import firebase from 'firebase';
@@ -9,9 +9,50 @@ export default class WelcomeScreen extends Component {
     super()
     this.state={
       emailId : '',
-      password: ''
+      password: '',
+      firstName: '',
+      lastName: '',
+      address: '',
+      contact: '',
+      confirmPassword: '',
+      isModalVisible: 'false'
     }
   }
+
+  userSignUp = (emailId, password, confirmPassword) =>{
+    if (password !== confirmPassword){
+      return Alert.alert("password does not match/ Kindly recheck.")
+    }else{
+       firebase.auth().createUserWithEmailAndPassord(emailId, password)     
+        .then(()=>{
+            db.collection('users').add({
+              first_Name:this.state.firstName,
+              last_name:this.state.lastName,
+              contact:this.state.contact,
+              email_id:this.state.emailId,
+              address:this.state.address
+            })
+       
+            return Alert.alert(
+               'User Added Successfully',
+               '',
+               [
+                 {text: 'OK', onPress: () => this.setState({"isModalVisible" :false})},
+               ]
+             
+            );
+       
+          })
+  
+    .catch((error) =>{
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      return Alert.alert(errorMessage)
+    });
+  }
+  
+}
+
 
   userLogin = (emailId, password)=>{
     firebase.auth().signInWithEmailAndPassword(emailId, password)
@@ -25,19 +66,117 @@ export default class WelcomeScreen extends Component {
     })
   }
 
-  userSignUp = (emailId, password) =>{
-    firebase.auth().createUserWithEmailAndPassword(emailId, password)
-    .then((response)=>{
-      return Alert.alert("Added Successfully")
-    })
-    .catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      return Alert.alert(errorMessage)
-    });
-  }
+  showModal = ()=>{
+    return(
+      <Modal 
+       animationType="fade"
+       transparent={true}
+       visible={this.state.isModalVisible}
+       >
+      <View style={styles.modalContainer}>
+        <ScrollView style={{width:'100%'}}>
+          <KeyboardAvoidingView style={styles.KeyboardAvoidingView}>
+            <Text
+            style={styles.modalTitle}
+            >Registration</Text>
+            <TextInput 
+             style={styles.formTextInput}
+             placeholder={"First Name"}
+             maxLength={8}
+             onChangeText={(text)=>{
+               this.setState({
+                 firstName: text
+               })
+             }}
+             />
+              <TextInput 
+             style={styles.formTextInput}
+             placeholder={"Last Name"}
+             maxLength={8}
+             onChangeText={(text)=>{
+               this.setState({
+                 lastName: text
+               })
+             }}
+             />
+            <TextInput 
+             style={styles.formTextInput}
+             placeholder={"Contact"}
+             maxLength={10}
+             keyboardType={'numeric'}
+             onChangeText={(text)=>{
+               this.setState({
+                 contact: text
+               })
+             }}
+             />
+              <TextInput 
+             style={styles.formTextInput}
+             placeholder={"Address"}
+             multiline={true}
+             onChangeText={(text)=>{
+               this.setState({
+                 address: text
+               })
+             }}
+             />
+            <TextInput 
+             style={styles.formTextInput}
+             placeholder={"Email"}
+             keyboardType={'email-address'}
+             onChangeText={(text)=>{
+               this.setState({
+                 emailId: text
+               })
+             }}
+             />
 
+           <TextInput 
+             style={styles.formTextInput}
+             placeholder={"Password"}
+             secureTextEntry={true}
+             onChangeText={(text)=>{
+               this.setState({
+                 password: text
+               })
+             }}
+             />
 
+          <TextInput 
+             style={styles.formTextInput}
+             placeholder={"Confirm Password"}
+             secureTextEntry={true}
+             onChangeText={(text)=>{
+               this.setState({
+                 confirmPassword: text
+               })
+             }}
+             />
+              <View style={styles.modalBackButton}>
+          <TouchableOpacity
+            style={styles.registerButton}
+            onPress={()=>
+              this.userSignUp(this.state.emailId, this.state.password, this.state.confirmPassword)
+            }
+          >
+          <Text style={styles.registerButtonText}>Register</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.modalBackButton}>
+          <TouchableOpacity
+            style={styles.cancelButton}
+            onPress={()=>this.setState({"isModalVisible":false})}
+          >
+          <Text style={{color:'#ff5722'}}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </View>
+  </Modal>
+)
+}
+  
   render(){
     return(
       <View style={styles.container}>
@@ -91,7 +230,9 @@ export default class WelcomeScreen extends Component {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:'#F8BE85'
+    backgroundColor:'#F8BE85',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   profileContainer:{
     flex:1,
@@ -112,6 +253,59 @@ const styles = StyleSheet.create({
     fontSize: 20,
     margin:10,
     paddingLeft:10
+  },
+  KeyboardAvoidingView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  modalTitle :{
+    justifyContent: 'center',
+    alignSelf: 'center',
+    fontSize:30,
+    color: '#ff5722',
+    marhin: 50
+  },
+  modalContainer: {
+    flex: 1,
+    borderRadius:20,
+    justifyContent: 'center',
+    backgroundColor: "#ffff",
+    marginRight: 30,
+    marginLeft: 30,
+    marginTop: 80,
+    marginBottom: 80,
+  },
+  formTextInput:{
+    width:"75%",
+    height:35,
+    alignSelf:'center',
+    borderColor:'#ffab91',
+    borderRadius:10,
+    borderWidth:1,
+    marginTop:20,
+    padding:10
+  },
+  registerButton:{
+    width:200,
+    height:40,
+    alignItems:'center',
+    justifyContent:'center',
+    borderWidth:1,
+    borderRadius:10,
+    marginTop:30
+  },
+  registerButtonText: {
+    color: '#ff5722',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  cancelButton: {
+    width: 200,
+    height: 30,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    marginTop: 5,  
   },
   button:{
     width:300,
