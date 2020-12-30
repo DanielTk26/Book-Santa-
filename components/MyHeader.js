c71a2800f35a6f4ba192f56d537ba91bc5358160
Daniel Thomas <63409207+DanielTk26@users.noreply.github.com>
2020-12-30 13:13:24 +0300

    Add files via upload

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -5,6 +5,7 @@ import  {View, Text, StyleSheet, Alert} from 'react-native';
 const MyHeader = props => {
     return(
         <Header
+         leftComponent={<Icon name='bars' type='font-awesome' color='#696969' onPress={() => props.navigation.toogleDrawer()}/>}
          centerComponent={{text: props.title, style: {color:'#90A5A9', fontSize:20,fontWeight:"bold", }}}
          backgroundColor = "#eaf8fe"
          />
